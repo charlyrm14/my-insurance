a84ca8446a27501262b431c3f9e919e0d578b0e2
charlyrm14 <charlyrm14@gmail.com>
2025-04-23 16:31:24 -0600

    feature: se agrega estructura de pagina de inicio

---
+++ src/components/home/Product.vue
@@ -0,0 +1,29 @@
+<script setup>
+</script>
+
+<template>
+    <div class="py-5 border-b-1 border-gray-100 dark:border-slate-700 mb-3 cursor-pointer hover:opacity-50">
+        <div class="flex justify-between gap-x-6">
+            <div class="flex min-w-0 gap-x-4 items-center">
+                <div class="border dark:border-slate-600 p-1 rounded-lg">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-7 text-red-500">
+                        <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+                    </svg>
+                </div>
+                <div class="min-w-0 flex-auto">
+                    <p class="uppercase text-sm dark:text-gray-100"> Protección familiar </p>
+                    <p class="uppercase text-sm dark:text-gray-300"> Plan advanced </p>
+                </div>
+            </div>
+            <div class="flex justify-between items-center gap-5">
+                <span class="bg-green-200 px-3 uppercase rounded-full text-green-600 text-sm"> Vigente </span>
+                <span
+                    class="cursor-pointer hover:opacity-75">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <path fill-rule="evenodd" d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z" clip-rule="evenodd" />
+                    </svg>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
--- src/views/home/HomeView.vue
+++ src/views/home/HomeView.vue
@@ -1,7 +1,53 @@
 <script setup>
-
+    import Product from '@/components/home/Product.vue'
 </script>
 
 <template>
-    
+    <div class="mt-4 space-y-4">
+
+        <p class="flex items-center uppercase gap-2"> 
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
+            </svg> Mis seguros 
+        </p>
+
+        <section>
+            <div class="flex justify-start gap-1">
+                <button
+                    class="bg-black text-white border px-3 py-1 rounded-full cursor-pointer hover:opacity-75 dark:bg-transparent dark:border-white"> 
+                        TODO 
+                </button>
+                <button
+                    class="bg-black text-white border px-3 py-1 rounded-full cursor-pointer hover:opacity-75 dark:bg-transparent dark:border-white"> 
+                        AXA 
+                </button>
+                <button
+                    class="bg-black text-white border px-3 py-1 rounded-full cursor-pointer hover:opacity-75 dark:bg-transparent dark:border-white"> 
+                        ZURICH 
+                </button>
+                <button
+                    class="bg-black text-white border px-3 py-1 rounded-full cursor-pointer hover:opacity-75 dark:bg-transparent dark:border-white"> 
+                        CHUBB 
+                </button>
+            </div>
+
+            <div class="mt-5">
+                <Product/>
+                <Product/>
+                <Product/>
+                <Product/>
+                <Product/>
+                <Product/>
+                <Product/>
+                <Product/>
+                <Product/>
+                <Product/>
+                <Product/>
+                <Product/>
+                <Product/>
+                <Product/>
+            </div>
+        </section>
+
+    </div>
 </template>
