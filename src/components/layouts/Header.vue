ed95860a5bb8f0fce76d9d9cab1762cb4a9ab67e
charlyrm14 <charlyrm14@gmail.com>
2025-04-23 14:00:28 -0600

    feature: vistas buscador, chat, shop y ajustes

--- src/components/layouts/Header.vue
+++ src/components/layouts/Header.vue
@@ -3,8 +3,8 @@
 </script>
 
 <template>
-    <header class="p-4 shadow z-20">
-        <p class="text-sm">Thursday 16th January</p>
-        <h1 class="text-xl font-bold">Welcome</h1>
+    <header class="p-2 shadow z-20">
+        <p class="text-sm"> Thursday 16th January </p>
+        <h1 class="text-lg font-bold"> Welcome </h1>
     </header>
 </template>
